1545e9c650c3029841ea26859c1bc82911e24d2d
anamwongk <kamonwan.anww@gmail.com>
2021-03-28 00:26:49 +0700

    update general route, base path

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import morgan from 'morgan'
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT
+const BASE_PATH = process.env.BASE_PATH
+const API_VERSION = process.env.API_VERSION
 
 const app = express()
 
@@ -11,7 +13,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(morgan('tiny'))
 
-app.use(routes)
+app.get('/', (req, res) => {
+  res.send({ success: true })
+})
+
+app.use(`${BASE_PATH}${API_VERSION}`, routes)
 
 app.listen(PORT, HOST, () => {
   console.log('Server is now running at port', PORT)
